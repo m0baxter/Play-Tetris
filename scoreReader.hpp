b0cfeb5e1350a7b434aca06f942ad6a4da8135cd
m0baxter <m0baxter@gmail.com>
2016-08-30 15:51:34 -0400

    Begin classifying ScoreReader.

---
+++ scoreReader.hpp
@@ -0,0 +1,30 @@
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/ml/ml.hpp>
+
+#ifndef SCORE_READER_HPP
+#define SCORE_READER_HPP
+
+class ScoreReader {
+
+   public:
+      //Public functions:
+      ScoreReader();
+      void trainReader( const int, const int, const char*);
+      void saveReader( const char* );
+      void loadReader( const char* );
+      int operator()( const int );
+
+   private:
+      //The SVM:
+      CvSVM svm;
+
+      //Private functions:
+      cv::Mat readLabels( const int, const char* );
+      std::vector<cv::Mat> getScoreDigits( const int );
+      cv::Mat genTrainingData( const int, const int );
+      float matToScore( const cv::Mat& );
+};
+
+#endif
+
